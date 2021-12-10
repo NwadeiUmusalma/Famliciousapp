136fbc3434940ff738cde1f4af59bb008b7b01c8
unknown <nwadeimusahumusalma@gmail.com>
2021-12-10 03:21:07 -0800

    Salma commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:famliciousapp/views/home_view.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FamliciousApp());
+}
+
+class FamliciousApp extends StatelessWidget {
+  const FamliciousApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Famlicious App',
+      theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          appBarTheme: AppBarTheme(
+            color: Colors.white,
+            elevation: 0,
+            foregroundColor: Colors.black,
+          )),
+      home: const HomeView(),
+    );
+  }
+}
